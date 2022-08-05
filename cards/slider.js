d1c7f2d6e9bd29a995d0292871eea8cada4b524d
Romver <roma_zverev2002@mail.ru>
2022-08-05 21:49:26 +0300

    Улучшил фунционал

--- cards/slider.js
+++ cards/slider.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide'); // NodeList(5) [div.slide, div.slide, div.slide, div.slide.active, div.slide]
+function slidesPlugin(itemActiveElement=0){
 
-for(const slide of slides){
-    slide.addEventListener('click',()=>{
-    clearActiveClasses();
-        slide.classList.add('active'); 
-    }); //addEventListener('название события',функция)
+    const slides = document.querySelectorAll('.slide'); // NodeList(5) [div.slide, div.slide, div.slide, div.slide.active, div.slide]
+
+    slides[itemActiveElement].classList.add('active');
+    for(const slide of slides){
+        slide.addEventListener('click',()=>{
+        clearActiveClasses();
+            slide.classList.add('active'); 
+        }); //addEventListener('название события',функция)
+    }
+
+    function clearActiveClasses(){
+        slides.forEach((slide)=>{
+            slide.classList.remove('active');
+        })
+    }
 }
 
-function clearActiveClasses(){
-    slides.forEach((slide)=>{
-        slide.classList.remove('active');
-    })
-}
+slidesPlugin(2);
